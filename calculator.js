e722ccabd0afea04c963bf9e30dc21bffdfda754
Tom <tom.terado@gmail.com>
2019-02-18 20:05:05 +0900

    Finished

--- calculator.js
+++ calculator.js
@@ -1,25 +1,31 @@
-function add () {
-	
+function add (num1, num2) {
+	return sum = num1 + num2;	
 }
 
-function subtract () {
-	
+function subtract (num1, num2) {
+	return sub = num1 - num2;	
 }
 
-function sum () {
-	
+function sum (array) {
+	return array.reduce((current, total) => total + current, 0);
 }
 
-function multiply () {
-	
+function multiply (array) {
+	return array.reduce((a,b) => a*b)
 }
 
-function power() {
-	
+function power(a,b) {
+	return Math.pow(a,b);
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if (a == 0 ){
+		return 1
+	}
+	for (var i = a - 1; i >= 1; i--){
+		a *= i;
+	}
+	return a;
 }
 
 module.exports = {
